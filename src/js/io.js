60560562d178cc15d0fb6e95a473df6b7bde5b9e
John Yee <hellojohnyee@gmail.com>
2017-02-17 16:33:00 -0800

    more progress

--- src/js/io.js
+++
@@ -1,1 +0,0 @@
-var IO = riot.observable();
---
+++ src/js/static_data.js
@@ -0,0 +1,63 @@
+/**
+* Static Data
+* For demo-ing components
+*/
+/** Nav Menu Items */
+var navMenuItems =  [
+	{ "name":"Home", "route":"/guide/contents","mdicon":"mdi mdi-home" },
+	{ "name":"Anchor Scroll", "route":"/guide/anchor-scroll","mdicon":"mdi mdi-anchor" },
+	{ "name":"Breakpoints", "route":"/guide/breakpoints","mdicon":"mdi mdi-cellphone-link" },
+	{ "name":"Buttons", "route":"/guide/buttons","mdicon":"mdi mdi-checkbox-multiple-blank" },
+	{ "name":"Charts", "route":"/guide/charts","mdicon":"mdi mdi-chart-pie" },
+	{ "name":"Flexbox", "route":"/guide/flexbox","mdicon":"mdi mdi-image-filter-center-focus" },
+	{ "name":"Fonts", "route":"/guide/fonts","mdicon":"mdi mdi-alphabetical" },
+	{ "name":"Form Elements", "route":"/guide/forms","mdicon":"mdi mdi-checkbox-multiple-marked" },
+	{ "name":"Grids", "route":"/guide/tables","mdicon":"mdi mdi-table-large" },
+	{ "name":"Layout Left", "route":"/guide/layout-left","mdicon":"mdi mdi-format-horizontal-align-left" },
+	{ "name":"Layout Right", "route":"/guide/layout-right","mdicon":"mdi mdi-format-horizontal-align-right" },
+	{ "name":"Maps", "route":"/guide/maps","mdicon":"mdi mdi-map" },
+	{ "name":"SVG Image Test", "route":"/guide/svg","mdicon":"mdi mdi-image" },
+	{ "name":"Theme", "route":"/guide/theme","mdicon":"mdi mdi-palette" }
+];
+/** Tab Bar Items */
+var tabBarItems = [
+	{ "name":"Tab One","active":true },
+	{ "name":"Tab Two","active":false },
+	{ "name":"Tab Three","active":false }
+]
+/** UI List Items */
+var uiListItems = [
+	{
+		"mdiavatar":"mdi mdi-account-circle",
+		"leftTop":"Everything",
+		"leftBot":"Subtitle",
+		"rightTop":"Value One",
+		"rightBot":"Subvalue",
+		"mdicon":"mdi mdi-check"
+	},
+	{
+		"mdiavatar":"mdi mdi-account-circle",
+		"leftTop":"No Icon",
+		"leftBot":"Subtitle",
+		"rightTop":"Value One",
+		"rightBot":"Subvalue",
+	},
+	{
+		"mdiavatar":"mdi mdi-account-circle",
+		"leftTop":"No Icon or Subvalue",
+		"leftBot":"Subtitle",
+		"rightTop":"Value One",
+	},
+	{
+		"mdiavatar":"mdi mdi-account-circle",
+		"leftTop":"No Icon, Value or Subvalue",
+		"leftBot":"Subtitle",
+	},
+	{
+		"mdiavatar":"mdi mdi-account-circle",
+		"leftTop":"No Icon, Value, Subvalue, or Subtitle",
+	},
+	{
+		"leftTop":"Text Only"
+	}
+]
