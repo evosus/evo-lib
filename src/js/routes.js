647ad462099d1192f5eac78c562197643175521f
John Yee <hellojohnyee@gmail.com>
2017-04-19 14:24:46 -0700

    removed unused route

--- src/js/routes.js
+++ src/js/routes.js
@@ -3,19 +3,9 @@
 // --------------------------------------
 // IMPORTANT: The '/' route MUST be LAST
 // --------------------------------------
-route('/layout-list', function() {
-	riot.compile(function() {
-		riot.mount('#appMount','app-template');
-		riot.mount('#rootMount','list-template');
-	});
-});
-// --------------------------------------
-// DEFAULT ROUTE
-// --------------------------------------
-// IMPORTANT: The '/' route MUST be LAST
-// --------------------------------------
 route('/', function() {
 	riot.compile(function() {
 		riot.mount('#appMount','app-template');
+		riot.mount('#rootMount','list-template');
 	});
 });
