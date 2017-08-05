cc175377464ef481b259729d94c3ba9beec36088
John Yee <hellojohnyee@gmail.com>
2017-08-04 17:20:49 -0700

    view-home working well as a blueprint

--- src/ui/ui-module.js
+++ src/ui/ui-module.js
@@ -6,16 +6,34 @@ var UserInterface = function() {
 	riot.observable(this);
 	var self = this, STATE = {};
 
+	// INIT STATE
+	self.initState = function(obj) {
+		var route = Object.keys(obj)[0],
+				STORED = self.getStore();
+		// IF route in storage
+		if(route && STORED) {
+			// SET STATE route from storage
+			STATE[route] = STORED[route];
+			// TRIGGER state change
+			self.stateChanged(route);
+		// ELSE route not in storage
+		} else {
+			// SET STATE and SET STORE
+			STATE[route] = obj[route];
+			// SET store
+			self.setStore(route);
+		}
+	};
 	// ERROR
 	self.error = function(req_event_name,opt_payload) {
 		console.error(req_event_name,opt_payload);
 	}
 	// STATE CHANGED
 	self.stateChanged = function(key) {
-		self.trigger(key + ':changed',STATE[key]);
+		self.trigger('UI_STATE_CHANGED',{ [key]:STATE[key] });
 	}
 	// GET STORE
-	self.getStore = function(key) {
+	self.getStore = function() {
 		//IF STATE in storage
 		if(stored = JSON.parse(localStorage.getItem('STATE'))) {
 			return stored;
@@ -25,6 +43,7 @@ var UserInterface = function() {
 	}
 	// SET STORE
 	self.setStore = function(key) {
+		console.info("SET STORE: " + key);
 		// IF stringify
 		if(val = JSON.stringify(STATE)) {
 			// SET STORAGE
@@ -34,27 +53,20 @@ var UserInterface = function() {
 	};
 	// SET STATE
 	self.setState = function(obj) {
-		// IF STATE in storage
-		if(stored = self.getStore()) {
-			// IF KEY in stored
-			if(stored[obj.key]) {
-				// REPLACE STATE
-				STATE = stored;
-				self.stateChanged(obj.key);
-			// ELSE KEY not in stored
-			} else {
-				STATE[obj.key] = obj;
-				self.setStore(obj.key);
-			}
-		// ELSE STATE not in storage
-		} else {
-			// SET KEY ON STATE
-			STATE[obj.key] = obj;
-			self.setStore(obj.key);
+
+		var route = Object.keys(obj)[0];
+
+		if(route) {
+			STATE[route] = obj[route];
+			self.stateChanged(route);
 		}
+
 	};
 	// EVENT HANDLERS
 	self.on('UI_SET_STATE',function(req_obj) {
 		self.setState(req_obj);
+	});
+	self.on('UI_INIT_STATE',function(req_obj) {
+		self.initState(req_obj);
 	});
 };
