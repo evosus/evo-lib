a0481b40862df2e26ce5143ab6d9dbf45d487009
John Yee <hellojohnyee@gmail.com>
2017-08-07 10:36:08 -0700

    8-7-2017

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@
 var preload = [
 	{ template:'nav-bar',url:'/nav/nav-bar.html' },
 	{ template:'nav-bar-logo',url:'/nav/nav-bar-logo.html' },
-	{ template:'nav-bar-form',url:'/nav/nav-bar-form.html' },
+	{ template:'nav-bar-detail',url:'/nav/nav-bar-detail.html' },
 	{ template:'nav-bar-tabs',url:'/nav/nav-bar-tabs.html' },
 	{ template:'nav-bar-icons',url:'/nav/nav-bar-icons.html' },
 	{ template:'nav-menu',url:'/nav/nav-menu.html' },
 	{ template:'ui-button',url:'/ui/ui-button.html' },
+	{ template:'ui-card',url:'/ui/ui-card.html' },
 	{ template:'ui-checkbox',url:'/ui/ui-checkbox.html' },
 	{ template:'ui-icon',url:'/ui/ui-icon.html' },
 	{ template:'ui-input',url:'/ui/ui-input.html' },
