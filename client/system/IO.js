9ce32c62312579dc88c3beb4dd17b5e214b48eca
John Yee <hellojohnyee@gmail.com>
2017-10-06 15:07:37 -0700

    IO and UI

---
+++ client/system/IO.js
@@ -0,0 +1,52 @@
+// --------------------------------------
+// INPUT OUTPUT MIXIN
+// --------------------------------------
+// Observable for tag IO control
+// --------------------------------------
+var IO  = {
+	init: function(opts) {
+		var self = this;
+		RC.on('UI_STATE_CHANGED',function(newState) {
+			if(newState[opts.title]) {
+				self.setOpts(newState[opts.title]);
+			}
+		});
+		this.on('update',function() {
+			self.updateRefs();	
+		});
+	},
+	checkState: function checkState(key) {
+		const STORED = JSON.parse(localStorage.getItem('STATE'));
+		if(STORED) {
+			Object.assign(this.opts, STORED[key]);
+			return STORED[key];
+		} else {
+			return false;
+		}
+	},
+	getOpts: function getOpts() { return this.opts },
+	setOpts: function setOpts(opts,update) {
+		this.opts = opts;
+		if(!update) {
+			this.update();
+			return this;
+		}
+	},
+	setState: function setState() {
+		if(this.opts && this.opts.title) {
+			RC.trigger('UI_SET_STATE',{ [this.opts.title]:this.opts });
+		}
+	},
+	updateRefs: function updateRefs() {
+		var self = this;
+		Object.keys(self.refs).forEach(function(REF) {
+			if(self.opts[REF]) {
+				if(self.refs[REF].hasOwnProperty('setOpts')) {
+					self.refs[REF].setOpts(self.opts[REF]);
+				}
+			}
+		});
+	}
+};
+
+export default IO;
---
+++ client/system/UI.js
@@ -0,0 +1,57 @@
+// --------------------------------------
+// UI MODULE
+// --------------------------------------
+var UserInterface = function() {
+	// OBSERVABLE
+	riot.observable(this);
+	// SELF & STATE
+	var self = this, STATE = {};
+	// ERROR
+	self.error = function(req_event_name,opt_payload) {
+		console.error(req_event_name,opt_payload);
+	}
+	// STATE CHANGED
+	self.stateChanged = function(key) {
+		self.trigger('UI_STATE_CHANGED',{ [key]:STATE[key] });
+	}
+	// GET STATEE
+	self.getState = function(opt_key) {
+		var STORED = JSON.parse(localStorage.getItem('STATE'));
+		if(STORED) {
+			if(STORED[opt_key]) {
+				return STORED[opt_key];
+			}	else {
+				return STORED;
+			}
+		} else {
+			return false;
+		}
+	}
+	// SET STORE
+	self.setStore = function(key) {
+		// IF stringify
+		const val = JSON.stringify(STATE);
+		if(val) {
+			// SET STORAGE
+			localStorage.setItem('STATE',val);
+			self.stateChanged(key);
+		}
+	};
+	// SET STATE
+	self.setState = function(obj) {
+		if(self.getState()) {
+			STATE = self.getState();
+		}
+		var route = Object.keys(obj)[0];
+		if(route) {
+			STATE[route] = obj[route];
+			self.setStore(route);
+		}
+	};
+	// EVENT HANDLERS
+	self.on('UI_SET_STATE',function(req_obj) {
+		self.setState(req_obj);
+	});
+};
+
+export default UserInterface;
