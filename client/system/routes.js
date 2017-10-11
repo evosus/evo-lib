d5b34db17a079a19cfbb6725cfcc811f8eeff720
John Yee <hellojohnyee@gmail.com>
2017-10-11 13:10:11 -0700

    updates wip

--- client/system/routes.js
+++ client/system/routes.js
@@ -4,6 +4,10 @@ import route from 'riot-route';
  */
 route('/',function() {
 	Promise.all([
+		import('client/system/tags/ui-banner.tag.html'),
+		import('client/system/tags/ui-button.tag.html'),
+		import('client/system/tags/ui-input.tag.html'),
+		import('client/system/tags/ui-toast.tag.html'),
 		import('client/app/app-template.tag.html')
 	]).then( () => {
 		riot.mount('#appMount','app-template');
