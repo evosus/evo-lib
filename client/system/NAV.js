854b93059346b132d364ce7c72941c44f27150e6
John Yee <hellojohnyee@gmail.com>
2017-10-12 15:52:29 -0700

    wip

---
+++ client/system/NAV.js
@@ -0,0 +1,25 @@
+/**
+ * Nav Menu Toggle
+ */
+export function navMenuToggle() {
+}
+/**
+ * Nav Menu Click
+ */
+export function navMenuClick() {
+}
+/**
+ * Banner
+ */
+export function banner() {
+}
+/**
+ * Toast
+ */
+export function toast() {
+}
+/**
+ * Modal
+ */
+export function modal() {
+}
