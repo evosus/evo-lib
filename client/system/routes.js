0676bb8fc44067cc102e6667d432df5c170150bf
John Yee <hellojohnyee@gmail.com>
2018-02-13 12:19:35 -0800

    wip adding ui-button-busy

--- client/system/routes.js
+++ client/system/routes.js
@@ -7,6 +7,7 @@ route('/',function() {
 	Promise.all([
 		import('client/system/tags/ui-select.tag.html'),
 		import('client/system/tags/ui-button.tag.html'),
+		import('client/system/tags/ui-button-busy.tag.html'),
 		import('client/system/tags/ui-input.tag.html'),
 		import('client/system/tags/nav-bar.tag.html'),
 		import('client/app/app-template.tag.html')
