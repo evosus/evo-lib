b37d31f694e009cf16f308bd62f9c7e2cfa97217
John Yee <hellojohnyee@gmail.com>
2018-05-02 18:13:18 -0700

    cleaned up with comments

--- client/system/tags/ui-mixin.js
+++ client/system/tags/ui-mixin.js
@@ -1,52 +1,35 @@
-import monitor from 'client/system/tags/ui-monitor.js';
+/**
+ * @file ui-mixin.js
+ * @description Provides reusable methods for ui components.
+ * @author John Yee <johny@evosus.com>
+ * @license ISC
+ * @copyright 2018 Evosus, Inc
+ */
 
-let UI = () => {
-	return {
-		model: [],
-		state: {},
-		getOpts() { return this.opts },
-		setOpts(opts,update) {
-			this.opts = opts;
-			if(!update) {
-				this.update();
-				return this;
-			}
-		}
-	};
-}
-export default UI;
 /*
-
-
-
-import monitor from 'client/system/tags/ui-monitor.js';
-let uiMixin = {
-	model: [],
-	init(opts) {
-		let self = this;
-
-		this.on('mount', () => {
-			Object.keys(this.refs).forEach( ref => {
-				this.model.push(ref);
-			});
-			console.info("TEMPLATE REFS COLLECTED");
-			console.dir(this.model);
-			this.state = monitor(this.model, (response) => {
-				if(response.ref in self.refs) {
-					self.refs[response.ref].setOpts(response.opts);
-				}
-			});
-		});
-
-	},
+ * UI
+ */
+const UI = {
+	/*
+	 * OBSERVABLE
+	 */
+	obs: riot.observable(),
+	/*
+	 * GET OPTS
+	 * @returns {object} this.opts
+	 */
 	getOpts() { return this.opts },
-	setOpts(opts,update) {
+	/*
+	 * SET OPTS
+	 */
+	setOpts(opts,preventUpdate) {
+
 		this.opts = opts;
-		if(!update) {
+
+		if(!preventUpdate) {
 			this.update();
 			return this;
 		}
 	}
-}
-export default uiMixin;
-*/
+};
+export default UI;
