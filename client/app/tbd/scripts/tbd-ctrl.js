303ba36e640898736305cb20d4766586aabf7fbc
troybevosus <troyb@evosus.com>
2018-06-14 17:13:27 -0700

    work with ag-grid, moving logic to tbd-ctrl

--- client/app/tbd/scripts/tbd-ctrl.js
+++ client/app/tbd/scripts/tbd-ctrl.js
@@ -1,11 +1,30 @@
+import * as agGrid from 'ag-grid';
+
+const columnDefs = [
+    {headerName: "Make", field: "make", checkboxSelection: true },
+    {headerName: "Model", field: "model"},
+    {headerName: "Price", field: "price"}
+]
+const gridOptions = {
+    columnDefs: columnDefs,
+    enableSorting: true,
+    enableFilter: true,
+    rowSelection: 'multiple'
+    
+}
 const ctrl = {
     init(opts) {
-        ctrl.opts = opts
-        ctrl.opts.title = 'tbd'
-        console.log('init ctrl');
-        this.initEvents(opts);
+        ctrl.opts = opts;
+		ctrl.opts.title = 'TBD';
+        ctrl.self = this;
+        this.gridOptions = gridOptions
+        ctrl.gridOptions = gridOptions
+		this.initEvents(opts)
     },
-    initEvents(opts) {
+    initEvents() {
+         // LIFECYCLE METHODS
+        // TODO: pick up where we left off w/ modules
+        //  currently gets this.on is not a function
         this.on('mount', () => {
             this.onMount();
         })
@@ -32,14 +51,30 @@ const ctrl = {
                 label: 'button2'
             }
         }
+        this.agGridSetUp()
         this.setState()
-    },
-    unMount() {
-        IO.default.deleteState(ctrl.opts.title)
     },
     setState() {
         IO.default.setState(ctrl.opts.title, ctrl.opts)
     },
+    unMount() {
+        IO.default.deleteState(ctrl.opts.title)
+    },
+    agGridSetUp() {
+
+        var eGridDiv = this.refs.ag_grid
+        console.log(gridOptions)
+        // create the grid passing in the div to use together with the columns & data we want to use
+        new agGrid.Grid(eGridDiv, gridOptions);
+        fetch('https://api.myjson.com/bins/15psn9').then((response) => {
+            return response.json();
+        }).then(function(data) {
+            gridOptions.api.setRowData(data);
+        })
+    },
+    // END OF LIFECYCLE METHODS
+
+    // STATE CHANGING METHODS
     buttonClick1() {
         ctrl.opts.nav_bar.title = 'cool new title'
         this.setState()
@@ -47,7 +82,22 @@ const ctrl = {
     buttonClick2() {
         ctrl.opts.nav_bar.title = 'cooler new title'
         this.setState()
+    },
+    getSelectedRows() {
+        //let gridOptions = this.refs.
+        console.log('getselectedrows called')
+        var selectedNodes = ctrl.opts.ag_grid.gridOptions.api.getSelectedNodes()  
+        var selectedData = selectedNodes.map( function(node) { return node.data })
+        var selectedDataStringPresentation = selectedData.map( function(node) { return node.make + ' ' + node.model }).join(', ')
+        alert('Selected nodes: ' + selectedDataStringPresentation);
+        this.setState()
     }
+    // END OF STATE CHANGING METHODS
+
+    // return {
+    //     buttonClick1,
+    //     buttonClick2
+    // }
 }
 
 export default ctrl;
